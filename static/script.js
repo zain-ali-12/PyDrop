623ab2b5a1a14468c0b692610efd87de575d1ee7
IGauto04 <63121360+zain-ali-12@users.noreply.github.com>
2022-03-23 00:18:38 +0400

    pop up animations

--- static/script.js
+++ static/script.js
@@ -1,6 +1,15 @@
 function activate(process) {
+    document.getElementById("blur-overlay").style.display = "block";
     if (process == 'send'){
-        document.getElementById("send-form").style.display = "flex";
-        document.getElementById("send-form").style.animation = 'grow 0.5s'
+        document.getElementById("send-form").id = "grow";
+    } else {
+        document.getElementById("recieve-form").id = "grow";
+
     }
+}
+
+function hideOpened() {
+    document.getElementById("send-form").id = ''
+    document.getElementById("recieve-form").id = ''
+    
 }
