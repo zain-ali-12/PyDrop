ba8715132d316cfb654072e5ab1909e17554c97d
IGauto04 <63121360+zain-ali-12@users.noreply.github.com>
2022-03-23 11:50:07 +0400

    animations working

--- static/script.js
+++ static/script.js
@@ -1,15 +1,28 @@
 function activate(process) {
     document.getElementById("blur-overlay").style.display = "block";
     if (process == 'send'){
-        document.getElementById("send-form").id = "grow";
+        document.getElementById("send-form").classList.add("grow");
     } else {
-        document.getElementById("recieve-form").id = "grow";
+        document.getElementById("recieve-form").classList.add("grow");
 
     }
 }
 
+function remClass(current) {
+    current.classList.remove('shrink');
+    current.classList.remove('grow');
+}
+
 function hideOpened() {
-    document.getElementById("send-form").id = ''
-    document.getElementById("recieve-form").id = ''
-    
+    let send = document.getElementById("send-form");
+    let recieve = document.getElementById("recieve-form");
+    let current = ''
+    if (send.classList.contains("grow")) {
+        current = send;
+    } else {
+        current = recieve;
+    }
+    current.classList.add("shrink");
+    current.addEventListener("animationend", remClass(current))
+    document.getElementById("blur-overlay").style.display = 'none';
 }
